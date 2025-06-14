e0fb77ba04b4255b92a6bd4b8b984eb184e2f903
Timoh97 <timohmugendi@gmail.com>
2025-06-14 06:37:50 +0300

    change titles

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Software Developer", "Applied Mathematician", "Designer", "Online Tutor"],
+        strings: ["Software Developer", "Applied Mathematician", "Writer", "Online Tutor"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Software Developer", "Applied Mathematician", "Designer", "Online Tutor"],
+        strings: ["Software Developer", "Applied Mathematician", "Writer", "Online Tutor"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
