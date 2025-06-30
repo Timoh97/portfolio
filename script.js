3c3c94de74b45ef6161366d95e978e9be857a646
Timoh97 <timohmugendi@gmail.com>
2025-06-30 04:36:47 +0300

    add projects

--- script.js
+++ script.js
@@ -1,70 +1,69 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        // sticky navbar on scroll
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
+
+        // scroll-up button show/hide
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
+    // scroll to top
+    $('.scroll-up-btn').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 'fast');
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
+    // smooth scroll on menu items click
+    $('.navbar .menu li a').click(function () {
         $('html').css("scrollBehavior", "smooth");
     });
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    // toggle navbar menu
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Software Developer", "Applied Mathematician", "Writer", "Online Tutor"],
+    // typing text animation
+    new Typed(".typing", {
+        strings: ["a Software Developer", "an Applied Mathematician", "a Writer", "an Online Tutor"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["Software Developer", "Applied Mathematician", "Writer", "Online Tutor"],
+    new Typed(".typing-2", {
+        strings: ["a Software Developer", "an Applied Mathematician", "a Writer", "an Online Tutor"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
-    // owl carousel script
+    // owl carousel
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
         autoplay: true,
-        autoplayTimeOut: 2000,
+        autoplayTimeout: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
